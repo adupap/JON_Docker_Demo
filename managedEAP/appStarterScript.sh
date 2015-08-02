14d17252fa5883deb0ffe562e99ae012e0089fad
adupap <adupap@gmail.com>
2015-08-02 19:37:11 -0400

    Update appStarterScript.sh

--- managedEAP/appStarterScript.sh
+++ managedEAP/appStarterScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#added & to start EAP in the background and using top commoand as a blocker, so that the container does not exit when EAP is shutdown from JON
 
 $HOME/eap/jboss-eap-6.1/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
 top
